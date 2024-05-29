2e38f67d84f623055e5d36ae63bc1e34cca6f7de
mosturera <164004752+mosturera@users.noreply.github.com>
2024-05-29 11:33:50 +0300

    lr.h

---
+++ lr.h
@@ -0,0 +1,18 @@
+#ifndef LR_H_INCLUDED
+#define LR_H_INCLUDED
+int pow(int x, int y);
+float put(float v1, float l);
+int ras(char* word);
+int lab1();
+int lab2();
+int lab3();
+int lab4();
+int lab5();
+int lab11();
+int lab21();
+int lab41();
+int lab51();
+int lab6();
+int lab6_1();
+int lab7();
+#endif
